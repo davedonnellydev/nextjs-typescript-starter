c28cbe689b67d1dddab4510aa01e698000566c16
Dave Donnelly <davepauldonnelly@gmail.com>
2025-07-29 23:26:19 +1000

    Update Home.test.tsx

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -4,6 +4,6 @@ import Home from '../src/app/page';
 describe('Home', () => {
   it('renders without crashing', () => {
     render(<Home />);
-    expect(screen.getByText(/Get started by editing/i)).toBeInTheDocument();
+    expect(screen.getByText(/Start building here./i)).toBeInTheDocument();
   });
 });
